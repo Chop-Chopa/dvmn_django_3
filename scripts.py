0be577bb4d6d2701d4f8f71c0b5526d643fc7e3e
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-08-18 21:32:17 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -35,38 +35,37 @@ COMPLIMENTS = [
     "Теперь у тебя точно все получится!"
 ]
 
-
-def fix_marks(schoolkid):
+def get_schoolkid(name):
     try:
-        child = Schoolkid.objects.get(full_name__contains=schoolkid)
-        student = Mark.objects.filter(schoolkid=child,points__in=['2','3']).update(points='5')
+        return Schoolkid.objects.get(full_name__contains=name)
     except Schoolkid.DoesNotExist:
         print("Ученик не найден. Введите корректное ФИО")
     except Schoolkid.MultipleObjectsReturned:
         print("Найдено несколько учеников. Уточните ФИО")
+    return None
+
+def fix_marks(schoolkid):
+    child = get_schoolkid(schoolkid)
+    if not child:
+        return
+    student = Mark.objects.filter(schoolkid=child,points__in=['2','3']).update(points='5')
 
 
 def remove_chastisements(schoolkid):
-    try:
-        child = Schoolkid.objects.get(full_name__contains=schoolkid)
-        deleted_count = Chastisement.objects.filter(schoolkid=child).delete()
-    except Schoolkid.DoesNotExist:
-        print("Ученик не найден. Введите корректное ФИО")
-    except Schoolkid.MultipleObjectsReturned:
-        print("Найдено несколько учеников. Уточните ФИО")
-
+    child = get_schoolkid(schoolkid)
+    if not child:
+        return
+    deleted_count = Chastisement.objects.filter(schoolkid=child).delete()
 
 def create_commendation(schoolkid, item):
-    try:
-        child = Schoolkid.objects.get(full_name__contains=schoolkid)
-        lessons = Lesson.objects.filter(year_of_study=child.year_of_study,group_letter=child.group_letter,subject__title=item)
-        if not lessons.exists():
-            print('Предмет не найден')
-            return
-        lesson = lessons.first()
-        random_compliment = random.choice(COMPLIMENTS)
-        commendation = Commendation.objects.create(text=random_compliment,created=lesson.date, schoolkid = child, subject = lesson.subject, teacher = lesson.teacher)
-    except Schoolkid.DoesNotExist:
-        print("Ученик не найден. Введите корректное ФИО")
-    except Schoolkid.MultipleObjectsReturned:
-        print("Найдено несколько учеников. Уточните ФИО")
+    child = get_schoolkid(schoolkid)
+    if not child:
+        return
+    lessons = Lesson.objects.filter(year_of_study=child.year_of_study,group_letter=child.group_letter,subject__title=item).order_by('-date')
+    if not lessons.exists():
+        print('Предмет не найден')
+        return
+    lesson = lessons.first()
+    random_compliment = random.choice(COMPLIMENTS)
+    commendation = Commendation.objects.create(text=random_compliment,created=lesson.date, schoolkid = child, subject = lesson.subject, teacher = lesson.teacher)
+
